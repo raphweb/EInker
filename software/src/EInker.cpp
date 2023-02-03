97c8228549ae6a3279cc74d583379a522ab0d701
raphweb <5662800+raphweb@users.noreply.github.com>
2023-02-03 13:58:18 +0000

    Made it work with ESP8266.

--- software/src/EInker.cpp
+++ software/src/EInker.cpp
@@ -5,45 +5,23 @@
 
 #include "DisplayConfig.hpp"
 
-/**************************** DEBUG *******************************/
-#define DEBUG
-
-#ifdef DEBUG
-#define DEBUG_PRINTLN(m) Serial.println(m)
-#define DEBUG_PRINT(m) Serial.print(m)
-
-#else
-#define DEBUG_PRINTLN(m)
-#define DEBUG_PRINT(m)
-
-#define DEBUG_PRINTLNC(m)
-#define DEBUG_PRINTC(m)
-#endif
-
 void setup() {
   Serial.begin(115200);
   while(!Serial) {}
-
-  // display setup and printing
-  display.init(115200, true, 20, false, *(new SPIClass(VSPI)), SPISettings(4000000, MSBFIRST, SPI_MODE0));
+  display.init(115200, true, 20, false);
   display.setRotation(1);
   display.setFullWindow();
   display.firstPage();
-  do {
-    display.fillScreen(GxEPD_WHITE);
-    display.setFont(&FreeSans12pt7b);
-    display.setTextColor(GxEPD_BLACK);
-    display.setCursor(25, 85);
-    display.print("Philipp ist dumm :-D");
-  } while (display.nextPage());
-
-  display.powerOff();
-
-  delay(1000);
-  Serial.flush();
-  esp_deep_sleep_start();
+  display.setFont(&FreeSans12pt7b);
+  display.setTextColor(GxEPD_BLACK);
 }
 
 void loop() {
-  // never executed when using deep sleep
+  do {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(25, 85);
+    display.print("ESP and EPaper is easy!");
+  } while (display.nextPage());
+  display.powerOff();
+  ESP.deepSleep(0);
 }
